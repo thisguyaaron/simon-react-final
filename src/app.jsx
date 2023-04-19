05467b2170791aaab983ce4b21c22b8ee983dbc4
thisguyaaron <azelenski123@gmail.com>
2023-04-18 21:55:32 -0600

    added my github link, name, and readme comments

--- src/app.jsx
+++ src/app.jsx
@@ -90,8 +90,8 @@ function App() {
 
       <footer className='bg-dark text-dark text-muted'>
         <div className='container-fluid'>
-          <span className='text-reset'>Author Name(s)</span>
-          <a className='text-reset' href='https://github.com/webprogramming260/simon-react'>
+          <span className='text-reset'>Author Name: Aaron Zelenski</span>
+          <a className='text-reset' href='https://github.com/thisguyaaron/simon-react-final'>
             Source
           </a>
         </div>
