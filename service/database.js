7175b81f7c4c167f643d9cc84a7992b114835852
thisguyaaron <azelenski123@gmail.com>
2023-04-18 21:03:10 -0600

    added my database

--- service/database.js
+++ service/database.js
@@ -10,7 +10,7 @@ if (!userName) {
   throw Error('Database not configured. Set environment variables');
 }
 
-const url = `mongodb+srv://${userName}:${password}@${hostname}`;
+const url = `mongodb+srv://thisguyaaron:<password>@cluster0.v6kbg.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(url);
 const userCollection = client.db('simon').collection('user');
